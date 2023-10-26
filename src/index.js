f9d5102cc06497dad03b24f3a5d7daa219feffc0
Faithlucy <cecygachanja5@gmail.com>
2023-10-26 18:10:11 +0300

    Code challenge

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 // Code here 
-const BASE_URL ="http://localhost:3000/beers"
 
-document.addEventListenera("DOMcontentloaded")
+document.addEventListener("DOMContentLoaded", ()=>{
+  fetch(`http://localhost:3000/beers`)
+  .then((response) => response.json())
+   .then((data) => {
+    const beer =data.find((beer) => beer.name ==="Oh So Flattening");
+    const beerName= beer.name;
+    const beerImage= "https://i.ibb.co/wQ4G0w1/flatiron-brew.png"
+    const beerDescription="A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once."
+    const beerRevews= [
+      "It's flat! Just the way I like it!!",
+      "Is this the real beer, is this just fantasy?",
+      "I've always hated bubbles going up my nose, so this is absolutely delightful.",
+      "Flat straight out of the bottle! No more waiting for the fizziness to subside. Thank you FlattaBeer!! I love you!!!"
+    ];
+    document.getElementsByClassName('beer-name')[0].textContent=beerName;
+    document.getElementsByClassName('beer-image')[0].src=beerImage;
+    document.getElementsByClassName('beer-description')[0].textContent=beerDescription;
 
-function seeBeerDetails (){
-  fetch(`${BASE_URL}`)
-  .then(response => response.json())
-  .then(data => {
-    const firstBeer = data[0]
 
-    const beerName= firstBeer.name;
-    const beerImage=firstBeer.image_url;
-    const beerDescription= firstBeer.description
-    const beerReviews= firstBeer.reviews.join(',');
+   })
 
-    document.getElementById('beer-name'),textContent = beerName;
-    document.getElementById('beer-image').src =beerImage;
-    document.getElementById('beer-description').textContent = beerDescription;
-    document.getElementById('beer-reviews').textContent=`Reviews : ${beer-reviews}`;
-  })
-  .catch(error => console.error ('Error fetching beer data:', error))
-}
-seeBeerDetails()
+})
 
 function seeMenu(){
-fetch(`${BASE_URL}`)
-.then(response => response.json())
-.then (data=> {
+fetch(`http://localhost:3000/beers`)
+.then((response)=> response.json())
+.then ((data)=> {
   const navElement= document.querySelector('nav');
   const beerMenu = document.createElement("ul");
 
@@ -50,13 +50,10 @@ function addNewReview(){
 
   const reviewInput = document.querySelector ("#review")
   const newReview = document.createElement ('p')
-  reviewElement.textContent= newReview;
-
+  newReview.textContent=reviewInput.value
   const reviewsContainer =document.querySelector('#review-list')
-  reviewsContainer.appendChild(reviewElement);
+  reviewsContainer.appendChild(newReview);
 
   reviewInput.value ='';
 }
-addNewReview(
-  
-)
+addNewReview()
